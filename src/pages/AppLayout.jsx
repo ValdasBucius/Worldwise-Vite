6f6806631a48664ecfeb754157e1924cb0ef05d4
Programeris3000 <valdasbucius1@gmail.com>
2023-12-12 11:27:44 +0200

    Files uploaded

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+
+      <p>Tekstas</p>
+    </div>
+  );
+}
+
+export default AppLayout;
